b849930f9a4011bb04eefc9ae723a6e774cbde2c
Nirmik Ukani <ukaninirmik65@gmail.com>
2025-10-29 11:12:47 +0530

    create slider and functionality

--- js/script.js
+++ js/script.js
@@ -4,41 +4,54 @@ const slides = document.querySelector(".slides");
 const slide = document.querySelectorAll(".slide");
 const pagination = document.querySelector(".pagination");
 
+
 let SlideNumber = 0;
 
+slide.forEach((s, index) => {
+  const paddingDiv = document.createElement("div");
+  const dotDiv = document.createElement("div");
+  paddingDiv.classList.add("dot-padding");
+  dotDiv.classList.add("dot");
 
-arrows_next.addEventListener("click", () => {
+  if (index === 0) {
+    dotDiv.classList.add("active");
+  }
+
+  dotDiv.addEventListener("click", () => {
+    SlideNumber = index;
+    slides.style.transform = `translateX(-${SlideNumber * 100}%)`;
+    updatePagination();
+  });
+
+  paddingDiv.appendChild(dotDiv);
+  pagination.appendChild(paddingDiv);
+
+});
+const dots = document.querySelectorAll('.dot')
+arrows_next.addEventListener("click", Next);
+
+arrows_prev.addEventListener("click", Back);
+
+function Next() {
   SlideNumber++;
   if (SlideNumber > slide.length - 1) {
     SlideNumber = 0;
   }
   slides.style.transform = `translateX(-${SlideNumber * 100}%)`;
- updatePagination()
-});
+  updatePagination();
+}
 
-arrows_prev.addEventListener("click", () => {
+function Back() {
   SlideNumber--;
-  if (SlideNumber < slide.length - 1) {
+  if (SlideNumber < 0) {
     SlideNumber = slide.length - 1;
   }
   slides.style.transform = `translateX(-${SlideNumber * 100}%)`;
- updatePagination()
-});
-
-slide.forEach((s, index) => {
-  const paddingDiv = document.createElement("div");
-  const dotDiv = document.createElement("div");
-
-  paddingDiv.classList.add("dot-padding");
-  dotDiv.classList.add("dot");
-
-  paddingDiv.appendChild(dotDiv);
-  pagination.appendChild(paddingDiv);
-
-  return dotDiv;
-});
+  updatePagination();
+}
 
 function updatePagination() {
-  const dots = document.querySelectorAll(".dot");
-  console.log(dots)
+  dots.forEach((dot, indx) => {
+    dot.classList.toggle("active", indx === SlideNumber);
+  });
 }
